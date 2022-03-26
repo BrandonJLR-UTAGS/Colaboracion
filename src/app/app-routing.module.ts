b56ee07cfcbecb6c445e8f86146886b402325275
Brandon de Jesus Lara Ruiz <190776@utags.edu.mx>
2022-03-25 22:49:10 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+//componentes
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
+import { SlidersComponent } from './sliders/sliders.component';
+import { TablaComponent } from './tabla/tabla.component';
+import { TarjetasComponent } from './tarjetas/tarjetas.component';
+
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/inicio', pathMatch:'full' },
+  { path: 'inicio', component:AppComponent},
+
+
+  // { path: 'inicio', component: SlidersComponent},
+  // { path: 'inicio', component: TarjetasComponent},
+  // {path: 'inicio', component: TablaComponent},
+  // { path: 'inicio', component: FooterComponent}
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { SlidersComponent } from './sliders/sliders.component';
+import { TarjetasComponent } from './tarjetas/tarjetas.component';
+import { TablaComponent } from './tabla/tabla.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SlidersComponent,
+    TarjetasComponent,
+    TablaComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
